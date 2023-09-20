f1f436e40490148fba6afa942905214b6850a84a
Dave <67056846+Deivid182@users.noreply.github.com>
2023-09-20 01:21:22 -0600

    fixed overlay

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,10 +15,11 @@ const Header = () => {
         onClose={() => setOpen(false)}
       />
       <div className='bg-hero'>
+        <div className='absolute inset-0 w-full h-[580px] lg:h-[750px] bg-black/50'/>
         <Container>
           <nav className='items-center justify-between py-6 flex'>
-            <h1 className='text-4xl font-bold text-white font-Alata'>loopstudios</h1>
-            <ul className='hidden lg:flex items-center gap-x-6'>
+            <h1 className='text-4xl font-bold text-white font-Alata z-10'>loopstudios</h1>
+            <ul className='hidden lg:flex items-center gap-x-6 z-10'>
               <li className='text-white text-lg tracking- font-Alata font-light hover:border-b hover:border-white p-2 transition-colors'>
                 <a href="#">About</a>
               </li>
@@ -48,8 +49,8 @@ const Header = () => {
               />
             </div>
           </nav>
-          <div className='flex items-start max-lg:justify-center flex-wrap mt-28 lg:mt-52'>
-            <div className='border-white border-2 max-lg:w-[95%] w-[600px] p-6 lg:p-12'>
+          <div className='flex items-start max-lg:justify-center flex-wrap mt-28 lg:mt-52 '>
+            <div className='border-white border-2 max-lg:w-[95%] w-[600px] p-6 lg:p-12 z-10'>
               <h2 className='text-white text-3xl lg:text-6xl uppercase font-Josefin font-medium'>
                 Inmersive experiences that deliver
               </h2>
